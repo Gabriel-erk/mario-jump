e7a99c78003fa3233d5ad9691d27b125f4528f93
Gabriel Erick <gabrielmmve13@gmail.com>
2024-03-22 18:14:03 -0300

    adicionando score

--- js/main.js
+++ js/main.js
@@ -6,6 +6,44 @@ const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const clouds = document.querySelector('.clouds');
 
+const scoreString = document.getElementById('score');
+
+// variavel que armazena o score inicial
+let scoreInt = 0;
+
+// defininDo que a variavel constante loopScore vai ter um loop, que vai aumentar o score de um em um, a cada 1 segundo
+// const loopScore = setInterval(() =>{
+//     scoreInt++;
+//     scoreString.textContent = scoreInt;
+// }, 1000); 
+
+// const startTime = Date.now();
+// const tempoDecorrido = Date.now() - startTime;
+
+function tempoScore() {
+
+    const loopScore = setInterval(() => {
+        scoreInt++
+        scoreString.textContent = scoreInt;
+    }, 1000);
+
+    setTimeout(() => {
+        clearInterval(loopScore);
+        const loopScore2 = setInterval(() =>{
+            scoreInt += 10;
+            scoreString.textContent = scoreInt;
+        }, 1000);
+
+        setTimeout(() =>{
+            clearInterval(loopScore2);
+            const loopScore3 = setInterval(() => {
+                scoreInt += 20;
+                scoreString.textContent = scoreInt;
+            }, 1000);
+        }, 10000); // apos 10 segundos adicionais (20 em 20)
+    }, 10000); // apos 10 segundos (10 em 10)
+
+}
 
 // criando função jump, que adiciona a classe jump do css (que executa a animação do mario, de pular)
 const jump = () => {
@@ -16,10 +54,12 @@ const jump = () => {
     /* sera criado a função setTimeout para resolver o problema atual, onde, a classe jump é adicionada e executada ao pressionar qualquer tecla do teclado, porem, ela é adicionada e executada apenas uma vez, nao se pode pular denovo, para isso, preciso remove-la apos 500 milisegundos (que é o tempo da animação), para poder executar novamente a animação sempre que eu quiser */
 
     /* chamando a função setTimeout, que recebe 2 parametros, uma função e o tempo que sera aguardado para executar a função - passei uma função anonima como parametro, que remove a classe jump - e o tempo a ser esperado, é de 500 milissegundos, que o tempo que minha animação dura */
-    setTimeout(()=> {
+    setTimeout(() => {
         mario.classList.remove('jump');
     }, 500);
 }
+
+tempoScore();
 
 // adicionando no html um escutador de evento do tipo kewydown (ou seja, quando baixar qualquer tecla),  e chamara a função jump)
 document.addEventListener('keydown', jump);
@@ -30,7 +70,7 @@ const loop = setInterval(() => {
     // basicamente, pipePosition recebe o valor esquerdo do pipe (recbe o left atual do cano)
     const pipePosition = pipe.offsetLeft;
 
-    /* resolvendo outro problema, para conseguir acessar o bottom do mario (pois, quero verificar se, o mario pulou sobre o tubo, a partir do seu bottom atual (a partir da altura do mario naquele momento)), para isso preciso acessar o bottom do mario, porem nao da forma que utilizei para left (ex: const marioPosition = mario.offsetBottom), pois me retorna um valor undefined*/ 
+    /* resolvendo outro problema, para conseguir acessar o bottom do mario (pois, quero verificar se, o mario pulou sobre o tubo, a partir do seu bottom atual (a partir da altura do mario naquele momento)), para isso preciso acessar o bottom do mario, porem nao da forma que utilizei para left (ex: const marioPosition = mario.offsetBottom), pois me retorna um valor undefined*/
 
     /*  getComputedStyle (metodo de window, que manipula a janela do navegador), pegara o estilo que foi computado na imagem do mario, e passamos o elemento que queremos pegar (agora podemos acessar qualquer propriedade css do elemento mario), no caso, 'mario' e logo, a propriedade alvo, bottom - utilizei o replace para remover o 'px' que aparece no console do navegador e substituir por nada '' (porem ainda continua como string o valor de marioPosition) - simbolo de + é para tentar converter o valor em number (para poder enxergar como numero no console do navegador e fazer futuros calculos) (agora marioPosition é um number que posso manipular)*/
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
@@ -41,7 +81,8 @@ const loop = setInterval(() => {
     /* utilizando a mesma coisa aqui, pois nao consegui acessar a propriedade right de clouds de outra maneira: const cloudsPosition = clouds.offsetRight (sempre retornava um valor undefined) */
     const cloudsPosition = +window.getComputedStyle(clouds).right.replace('px', '');
 
-    // se a posição esquerda do tubo for 120 e a posição do tubo for maior que 0 (ou seja, o tubo ainda esta abaixo do mario, em outras palavras, o tubo ainda nao saiu da tela, quando ele sair da tela(ou seja, quando sua posição for menor que 0, pois 0 é a posição em que o tubo nao aparece mais) significa que o tubo nao vai mais estar embaixo do mario, e nao tem como ele cair e acertar o tubo, ja que, se parar para reparar, a posição do mario, nao muda, somente a do tubo, e enquanto o tubo estiver na posição acima de 0 o mario pode acertar o tubo) e a posição do mario menor que 80 (no caso, se a altura que o mario esta for menor que 80, significa que ele atingiu o tubo) o jogo acaba
+    /* se a posição esquerda do tubo for 120 e a posição do tubo for maior que 0 (ou seja, o tubo ainda esta abaixo do mario, em outras palavras, o tubo ainda nao saiu da tela, quando ele sair da tela(ou seja, quando sua posição for menor que 0, pois 0 é a posição em que o tubo nao aparece mais) significa que o tubo nao vai mais estar embaixo do mario, e nao tem como ele cair e acertar o tubo, ja que, se parar para reparar, a posição do mario, nao muda, somente a do tubo, e enquanto o tubo estiver na posição acima de 0 o mario pode acertar o tubo) e a posição do mario menor que 80 (no caso, se a altura que o mario esta for menor que 80, significa que ele atingiu o tubo) o jogo acaba */
+    // marioPosition < 80 = altura minima do mario para nao acertar o tubo
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
 
         /* definindo que, quando o left de pipe for menor ou igual a 120px (left é como o rigth, top, bottom, propriedades de position: absolute, quando left chega em 120px ou menos, ele ja esta em contato com a img do mario, oq significa que o jogo acabou, vc perdeu, e quando isso acontecer, preciso que a animação pare) */
@@ -49,7 +90,7 @@ const loop = setInterval(() => {
         //.style.animation acessa a propriedade animetion do estilo css aplicado ao elemento pipe (no caso a imagem do tubo) = 'none', atribui o valor none a propriedade animation, que significa que qualquer animação atualmente aplicada ao elemento sera removida
         pipe.style.animation = 'none';
 
-        /* corrigindo outro problema, que, ao acertar o mario, o tubo volta para sua posição original, no canto inferior esquerdo da tela, que nao quero que aconteça, quero que o tubo permaneça na posição onde atingiu o mario*/ 
+        /* corrigindo outro problema, que, ao acertar o mario, o tubo volta para sua posição original, no canto inferior esquerdo da tela, que nao quero que aconteça, quero que o tubo permaneça na posição onde atingiu o mario*/
 
         // alterando a posição do tubo para a mesma posição em que atingiu o mario (pipePosition guarda o valor em pixels do tubo onde mario foi atingido, entao coloco sua posição atual, para onde mario foi atingido)
         pipe.style.left = `${pipePosition}px`;
@@ -74,5 +115,6 @@ const loop = setInterval(() => {
 
         /* cancelando a execulçao do intervalo de tempo em que o loop sera executado(verificando continuamente as posições do tubo e do mario, que acaba se repetindo ifinitamente quando as condições sao atendidas), passando o identificador do intervalo que quero cancelar, que esta armazenado dentro da variavel constante chamada 'loop' (ou seja, depois da função ser executada uma vez, nao sera executada novamente apos 10 milisegundos - o loop para de rodar)*/
         clearInterval(loop);
+        clearInterval(loopScore3);
     }
 }, 10);
